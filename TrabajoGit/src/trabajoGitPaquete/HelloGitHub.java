c79d97ca55273b0099b1db8c403bc1fc8ca69aeb
Jaime Gamero Matos <gameromatos@gmail.com>
2026-01-20 17:56:48 +0100

    esta es la actualizacion añadiendo una funcion a el proyecto a traves de una rama

--- TrabajoGit/src/trabajoGitPaquete/HelloGitHub.java
+++ TrabajoGit/src/trabajoGitPaquete/HelloGitHub.java
@@ -6,5 +6,9 @@ public class HelloGitHub {
 		// TODO Auto-generated method stub
 		System.out.println("hola github"+"vamos que le estas añadiendo cosas no jodas");
 	}
-
+	public static int sumar(int numero1, int numero2) {
+		int suma=0;
+		suma= numero1 +numero2;
+		return suma;
+	}
 }
